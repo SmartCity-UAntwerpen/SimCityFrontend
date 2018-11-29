a84ee060f6209067db61c85ef49f94e297ae6621
Imre Liessens <imre.liessens@telenet.be>
2018-11-29 17:09:39 +0100

    Load workers from file when dev profile

--- src/main/java/be/uantwerpen/sc/models/sim/SimWorkerType.java
+++ src/main/java/be/uantwerpen/sc/models/sim/SimWorkerType.java
@@ -3,5 +3,14 @@ package be.uantwerpen.sc.models.sim;
 public enum SimWorkerType {
     car,
     f1,
-    drone
+    drone;
+
+    public static SimWorkerType StringToType(String type) {
+        switch (type.toUpperCase()) {
+            case("CAR"): return  SimWorkerType.car;
+            case("F1"): return  SimWorkerType.f1;
+            case("DRONE"): return  SimWorkerType.drone;
+            default: throw new IllegalArgumentException("Unkwown worker type");
+        }
+    }
 }
