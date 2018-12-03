50e70789ab48d55593bf424a844f0613d40545b5
Imre Liessens <imre.liessens@telenet.be>
2018-12-03 21:17:11 +0100

    Fixed test for F1Backend

--- src/main/java/be/uantwerpen/sc/services/vehicleBackends/F1Backend.java
+++ src/main/java/be/uantwerpen/sc/services/vehicleBackends/F1Backend.java
@@ -2,36 +2,45 @@ package be.uantwerpen.sc.services.vehicleBackends;
 
 import be.uantwerpen.sc.models.sim.messages.F1WayPoint;
 import be.uantwerpen.sc.models.sim.messages.F1WayPointMap;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class F1Backend {
 
+    @Value("${backend.f1.url}")
     private String backendURL;
+
+    @Autowired
     private RestTemplate restTemplate;
     private ObjectMapper mapper = new ObjectMapper();
 
-    public F1Backend(@Value("${backend.f1.url") String backendURL) {
-        this.backendURL = backendURL;
-        restTemplate = new RestTemplate();
-    }
-
-
-    public Map<Long,F1WayPoint> getWayPoints() throws IOException {
+    public Map<Long,F1WayPoint> getWayPoints() {
         String endpoint = backendURL + "getwaypoints";
 
         F1WayPointMap data = restTemplate.getForObject(endpoint,F1WayPointMap.class);
 
-        return null;
+        return data.getWayPoints();
+    }
+
+    public String getBackendURL() {
+        return backendURL;
+    }
+
+    public void setBackendURL(String backendURL) {
+        this.backendURL = backendURL;
+    }
+
+    // define REST bean
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
